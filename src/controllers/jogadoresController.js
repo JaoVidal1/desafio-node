402eaf0fffdc4443d6ecb0598eb0d3788e981414
JaoVidal1 <jaovidal1@gmail.com>
2022-05-10 19:24:22 -0300

    att

--- src/controllers/jogadoresController.js
+++ src/controllers/jogadoresController.js
@@ -4,20 +4,20 @@ class JogadorController {
 static listarJogadores = (req, res) => {
     jogadores.find((err, jogadores) => {
         res.status(200).json(jogadores)
-})
-}
-static listarJogadorPorID = (req, res) => {
+});
+};
+static listarJogadorPorID = (req, res => {
     const id = req.params.id;
 
-    jogadores.findById(id, (err, jogadores)) => {
+    jogadores.findById(id, (err, jogadores => {
         if(err){
             res.status(400).send({message: '${err.message} - Id do jogador não localizado.'}
             )}
 else {
     res.status(200).send(jogadores);
-}
-        }
-    }
+};
+ }));
+});
 
 static cadastrarJogador = (req, res) => {
     let jogador = new jogadores(req.body);
@@ -28,7 +28,7 @@ static cadastrarJogador = (req, res) => {
         } else{
             res.status(201).send(jogador.toJSON())
         }
-    })
+    })};
 static atualizarJogador = (req, res)=> {
     const id = req.params.id;
 
@@ -38,9 +38,9 @@ static atualizarJogador = (req, res)=> {
         } else{
             res.status(500).send({message: err.message})
         }
-    })
+    });
 }
-}
+
 static excluirJogador = (req, res) =>{
     const id = req.params.id;
 
@@ -50,20 +50,23 @@ static excluirJogador = (req, res) =>{
         } else {
             res.status(500).send({message: err.message})
         }
-    })
-}
+    });
+};
 static premioJogador = (req, res) =>{
-    const jogador = req.params.id;
+    const id = req.params.id;
 
-    jogadores.findByIdAndUpdate(jogador) => {
+    const jogadoresResponse = await jogadores.find()
+    jogadoresResponse.forEach(jogador => {
         const moeda = jogador.moedas;
         let medalhas = parseInt(moedas / 10)
         let trofeus = parseInt(medalhas / 3)
 
         jogador.medalha = medalhas
         jogador.trofeu = trofeus
-    }
+    });
+      res.json(jogadoresResponse)
+    };
 
-}
-}
+};
+
 export default JogadorController
---
+++ src/models/Moedas.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const moedaSchema = new mongoose.Schema(
+    {
+        moedas: {type: Number}
+    }
+)
