5093f43f919696cb7e3143a9e8c1f52460cb38c4
JaoVidal1 <jaovidal1@gmail.com>
2022-05-11 13:39:25 -0300

    att

--- src/routes/jogadoresRoutes.js
+++ src/routes/jogadoresRoutes.js
@@ -7,7 +7,7 @@ router
 .get("/jogadores", JogadorController.listarJogadores)
 .get("/jogadores/:id", JogadorController.listarJogadorPorID)
 .post("/jogadores", JogadorController.cadastrarJogador)
-.put("/livros/:id", JogadorController.atualizarJogador)
+.put("/jogadores/:id", JogadorController.atualizarJogador)
 .delete("/jogadores/:id", JogadorController.excluirJogador)
 .put("/jogadores/:id", JogadorController.premioJogador)
 export default router;
