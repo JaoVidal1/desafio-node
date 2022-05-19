b0e61e1ca4b2072e0fee96bd129dab1b18613f7d
JaoVidal1 <jaovidal1@gmail.com>
2022-05-19 14:44:35 -0300

    fix

--- src/tests/tests-spec..js
+++ src/tests/tests-spec..js
@@ -50,4 +50,58 @@ describe('Testes da funcionalidade da api', async () => {
       }
     ])
   })
+  it('should return an empty array if the I dont have any data saved on database', async () =>{
+ 
+    sandbox.stub(repository, "jogadorBusca").resolves([]);
+
+    const response = await services.lerJogador();
+ 
+    console.log('response', response) 
+    expect(response).to.be.deep.equal([]);
+  })
+  it('should confirm if the method jogadorDelete is called', async () => {
+ 
+    const stubDelete = sandbox.stub(repository, "jogadorDeleta")
+ 
+    await services.deleteJogador()
+
+    sinon.assert.calledOnce(stubDelete);
+  })
+
+  it('Get jogadores - status', () =>{
+    chai.request('http://localhost:3000')
+      .get('/jogadores')
+      .end(function(err, res) {
+        expect(res).to.have.status(201);   
+      });
+  });
+  it.skip('Post jogadores - status', () =>{
+    chai.request('http://localhost:3000')
+      .post('/jogadores')
+      .send({
+        "name" : "Lucio",
+      	"coins" : 143
+      })
+      .end((err, res) => {
+        chai.expect(res).to.have.status(201); 
+      });
+  });
+  it('Put jogadores - status', () =>{
+    chai.request('http://localhost:3000')
+      .put('/jogadores/628414e908b7922aab930001')
+      .send({
+      	"coins" : 400
+      })
+      .end((err, res) => {
+        chai.expect(res).to.have.status(201); 
+      });
+  });
+  
+  it.skip('Delete jogadores - status', () =>{
+    chai.request('http://localhost:3000')
+      .delete('/jogadores/')
+      .end((err, res) => {
+        chai.expect(res).to.have.status(201); 
+      });
+  });
 })
